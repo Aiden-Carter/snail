99df30db170230fc3b77827b1d3478dfbdbfe8b6
Your Name <houchun666@sina.com>
2019-08-19 11:29:54 +0800

    init repo

---
+++ src/main/java/com/example/carteraiden/demotry/DemoTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.carteraiden.demotry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTryApplication.class, args);
+    }
+
+}
