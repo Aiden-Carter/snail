25e41cc64c80f956db5e668e7f8c3d3813589f8d
Your Name <houchun666@sina.com>
2019-08-19 20:19:15 +0800

    add index and bootstrap

--- src/main/java/com/example/carteraiden/demotry/controller/IndexldController.java
+++ src/main/java/com/example/carteraiden/demotry/controller/IndexldController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloWorldController {
+public class IndexldController {
     //前台模块的名
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String indxe(){
+
+        return "index";
         //对应html文件
     }
 }
